8fb63c01d30bf1e43d8a9001f6c2ffd1ffdbcf97
Francivaldo Alves <francivaldo.alves@gmail.com>
2023-03-19 09:35:10 -0300

    crud estado

---
+++ backend/src/main/java/com/f3pro/backend/repository/EstadoRepository.java
@@ -0,0 +1,10 @@
+package com.f3pro.backend.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.f3pro.backend.entity.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado,Long>{
+
+    
+}
